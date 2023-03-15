1ec1cf664f9329e4a339dd7dead7102ea348c9b2
olegjirmont <126573555+olegjirmont@users.noreply.github.com>
2023-03-15 17:35:31 +0100

    Domwork.java ,  Person.java
    Homework 0 , 1

---
+++ Domwork.java
@@ -0,0 +1,48 @@
+public class Domwork {
+    public static void main(String[] args) {
+
+        int a = 1144;
+
+        System.out.println(1144 / 8);
+        System.out.println(1144 % 8);
+        System.out.println(143 / 8);
+        System.out.println(143 % 8);
+        System.out.println(17 / 8);
+        System.out.println(17 % 8);
+        System.out.println(2 / 8);
+        System.out.println(2 % 8);   //2170
+
+        int a8 = 02170;
+        System.out.println(a8);
+        System.out.println(2 * Math.pow(8, 3) + 1 * Math.pow(8, 2) + 7 * Math.pow(8, 1));
+
+        int b = 678;
+
+        System.out.println(678 / 8);
+        System.out.println(678 % 8);
+        System.out.println(84 / 8);
+        System.out.println(84 % 8);
+        System.out.println(10 / 8);
+        System.out.println(10 % 8);
+        System.out.println(1 / 8);
+        System.out.println(1 % 8); //1246
+
+        int b8 = 01246;
+        System.out.println(b8);
+        System.out.println(1 * Math.pow(8, 3) + 2 * Math.pow(8, 2) + 4 * Math.pow(8, 1) + 6 * Math.pow(8, 0));
+
+        int c = 12;
+
+        System.out.println(12 / 8);
+        System.out.println(12 % 8);
+        System.out.println(1 / 8);
+        System.out.println(1 % 8);  //14
+
+        int c8 = 014;
+        System.out.println(c8);
+        System.out.println(1 * Math.pow(8, 1) + 4 * Math.pow(8, 0));
+
+
+    }
+
+}
---
+++ Person.java
@@ -0,0 +1,30 @@
+public class Person {
+    public static void main(String[] args) {
+        String name = "Oleg";
+        int year = 1966;
+
+        System.out.println(name + year);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
